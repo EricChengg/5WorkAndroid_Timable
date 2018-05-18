2d165107bc4b16f3692a5fd71f8a15eb9bc1282d
EEEriccc <lung786@gmail.com>
2018-05-18 17:52:59 +0930

    Creating SQLite database.

--- Personal_Timetable_Android/app/src/main/java/au/edu/tafesa/itstudies/personal_timetable_android/DetailActivity.java
+++ Personal_Timetable_Android/app/src/main/java/au/edu/tafesa/itstudies/personal_timetable_android/DetailActivity.java
@@ -6,6 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.*;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import java.sql.Time;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -24,5 +37,10 @@ public class DetailActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void DetailActivity()
+    {
+        ClassInfo classInfo;
     }
 }
