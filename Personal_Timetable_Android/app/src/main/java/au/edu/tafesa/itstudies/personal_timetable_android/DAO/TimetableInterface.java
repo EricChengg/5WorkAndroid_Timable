77961fafe8a8711f2480152dc54265be863ff313
EricChengg <36833294+EricChengg@users.noreply.github.com>
2018-11-01 21:53:28 +1030

    inspected code
    inspected code

--- Personal_Timetable_Android/app/src/main/java/au/edu/tafesa/itstudies/personal_timetable_android/DAO/TimetableInterface.java
+++
@@ -1,9 +0,0 @@
-package au.edu.tafesa.itstudies.personal_timetable_android.DAO;
-
-import java.util.List;
-
-import au.edu.tafesa.itstudies.personal_timetable_android.models.Student;
-
-public interface TimetableInterface {
-
-}
